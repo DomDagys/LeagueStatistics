60496e8a8b6a1a97a31ad58bd77cea860d5d7763
DomDagys <dominykasnamu@gmail.com>
2020-02-25 00:02:28 +0200

    Adding api project

---
+++ LeagueStatisticsApi/LeagueStatistics/Api Configurations/DependencyInjections.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueStatistics.Api_Configurations
+{
+    public class DependencyInjections
+    {
+
+    }
+}
---
+++ LeagueStatisticsApi/LeagueStatistics/Api Configurations/StartupExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+
+namespace LeagueStatistics.Api_Configurations
+{
+    public static class StartupExtensions
+    {
+        public static IServiceCollection SwaggerServiceExtension(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("leaguestats", new OpenApiInfo { Title = "LeagueStatistics", Version = "v1" });
+            });
+
+            return services;
+        }
+
+        public static void SwaggerConfigurationExtension(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/swagger/leaguestats/swagger.json", "LeagueStatistics");
+                options.RoutePrefix = "leaguestats";
+            });
+        }
+    }
+}
