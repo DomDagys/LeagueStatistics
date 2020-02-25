c6c85c4499fcc4715e168caffcc729bcae23def0
gintautassedys <gintautassedys@outlook.com>
2020-02-25 23:12:16 +0200

    NavBar 1.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import LoginForm from './components/loginForm/LoginForm';
+import NavigationBar from './components/navigation/NavigationBar'
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar/>
       <h1>Hello World!</h1>
       <LoginForm/>
     </div>
---
+++ frontend/src/components/navigation/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavigationBar() {
+  return(
+    <nav className="navbar navbar-dark bg-primary fixed-right">
+      <a href="https://app.mobalytics.gg/summoner-search">Home</a>
+      <a>Profile</a>
+      <a href="https://en.wikipedia.org/wiki/React_(web_framework)">Ect</a>
+    </nav>
+  )
+}
+
+export default NavigationBar;
