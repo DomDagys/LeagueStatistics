0035a282ae9e62b0e14322a1f989e246097498d2
Darius Kulevicius <dariuux@gmail.com>
2020-02-25 20:35:02 +0200

    Added onChange on LoginForm.js

--- frontend/src/components/loginForm/LoginForm.js
+++ frontend/src/components/loginForm/LoginForm.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React from "react";
 
 class LoginForm extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       username: "",
-      password: "",
-    }
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleChange(e) {
+    console.log(e);
+  }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <h1>Login</h1>
-        <form onSubmit=>
-          <input value={this.state.username} type="username" required placeholder="Enter user name"/>
-          <input type="pasword" required placeholder="Enter pasword"/>
-          <input type="submit"/>
+        <form>
+          <input
+            value={this.state.username}
+            type="username"
+            required
+            placeholder="Enter user name"
+            onChange={this.handleChange}
+          />
+          <input
+            type="pasword"
+            required
+            placeholder="Enter pasword"
+            onChange={this.handleChange}
+          />
+          <input type="submit" />
         </form>
       </div>
     );
