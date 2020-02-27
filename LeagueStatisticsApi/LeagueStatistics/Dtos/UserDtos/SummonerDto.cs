bec42cecaf67de3a27cbcc09a7710f19aa5ca5fc
Pakaz1 <61023046+Pakaz1@users.noreply.github.com>
2020-02-27 22:47:59 +0200

    Added a Summoners Dto

---
+++ LeagueStatisticsApi/LeagueStatistics/Dtos/UserDtos/SummonerDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueStatistics.Dtos.UserDtos
+{
+    public class SummonerDto
+    {
+        public int ProfileIconID { get; set; }
+        public string SummonerName { get; set; }
+        public long SummonerLevel { get; set; }
+    }
+}
