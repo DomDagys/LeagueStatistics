e02f5201d97220bf82f51e84782d5d68d1f7d395
Pakaz1 <61023046+Pakaz1@users.noreply.github.com>
2020-02-27 22:48:11 +0200

    Added a SummonerController

---
+++ LeagueStatisticsApi/LeagueStatistics/Controllers/SummonerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LeagueStatistics.Services.RiotAPI;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeagueStatistics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SummonerController : ControllerBase
+    {
+        // GET: api/Summoner
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Summoner/5
+        [HttpGet("{id}")]//, Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Summoner
+        [HttpPost]
+        public string Post(string value)
+        {
+            Summoner_V4 Summoner = new Summoner_V4("EUN1");
+
+            var summoner = Summoner.GetSummonerByName(value);
+            //return "PEtras";
+            return summoner.SummonerName + " " + summoner.SummonerLevel + " " + summoner.ProfileIconID;
+        }
+
+        // PUT: api/Summoner/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
