6d29706723824b6aa14c215206fa233f68f4cab3
Pakaz1 <61023046+Pakaz1@users.noreply.github.com>
2020-03-03 22:58:07 +0200

    Updated Summoner_V4

--- LeagueStatisticsApi/LeagueStatistics/Services/RiotAPI/Summoner_V4Service.cs
+++ LeagueStatisticsApi/LeagueStatistics/Services/RiotAPI/Summoner_V4Service.cs
@@ -3,34 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LeagueStatistics.Dtos.SummonerDtos;
+using LeagueStatistics.Services.Interfaces;
 using Newtonsoft;
 using Newtonsoft.Json.Linq;
 
 namespace LeagueStatistics.Services.RiotAPI
 {
-    public class Summoner_V4Service : GeneralAPI
+    public class Summoner_V4Service : GeneralAPI, ISummoner_V4Service
     {
-        public Summoner_V4Service(string region) : base(region)
+        public Summoner_V4Service()
         {
 
         }
 
-        public SummonerDto GetSummonerByName(string SummonerName)
+        public SummonerDto GetSummonerByName(string SummonerName, string region)
         {
             string path = "summoner/v4/summoners/by-name/" + SummonerName;
 
-            var response = GET(GetURI(path));
+            var response = GET(GetURI(path, region));
             string content = response.Content.ReadAsStringAsync().Result;
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                JToken token = JObject.Parse(content);
-
-                SummonerDto summoner = new SummonerDto();
-                summoner.ProfileIconID = (int)token.SelectToken("profileIconId");
-                summoner.SummonerName = (string)token.SelectToken("name");
-                summoner.SummonerLevel = (long)token.SelectToken("summonerLevel");
-                return summoner;
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<SummonerDto>(content);
             }
             else
             {
