f9ee405bd8a087d21b0cb40e3890c1be3169e696
Pakaz1 <61023046+Pakaz1@users.noreply.github.com>
2020-03-03 22:57:03 +0200

    Added interface for Summonver_V4

---
+++ LeagueStatisticsApi/LeagueStatistics/Services/Interfaces/ISummoner_V4Service.cs
@@ -0,0 +1,13 @@
+﻿using LeagueStatistics.Dtos.SummonerDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueStatistics.Services.Interfaces
+{
+    interface ISummoner_V4Service
+    {
+        SummonerDto GetSummonerByName(string SummonerName, string region);
+    }
+}
