790befe17d4f7ac1661e85d54f732d542e4c8d0f
Pakaz1 <61023046+Pakaz1@users.noreply.github.com>
2020-03-03 22:56:41 +0200

    Updated SummonerDto

--- LeagueStatisticsApi/LeagueStatistics/Dtos/SummonerDtos/SummonerDto.cs
+++ LeagueStatisticsApi/LeagueStatistics/Dtos/SummonerDtos/SummonerDto.cs
@@ -7,8 +7,12 @@ namespace LeagueStatistics.Dtos.SummonerDtos
 {
     public class SummonerDto
     {
-        public int ProfileIconID { get; set; }
-        public string SummonerName { get; set; }
-        public long SummonerLevel { get; set; }
+        public int profileIconId { get; set; }
+        public string name { get; set; }
+        public string puuid { get; set; } //Encrypted PUUID
+        public long summonerLevel { get; set; }
+        public long revisionDate { get; set; }
+        public string id { get; set; } //Enctrypted summoner ID
+        public string accountId { get; set; } //Encrypted account ID
     }
 }
