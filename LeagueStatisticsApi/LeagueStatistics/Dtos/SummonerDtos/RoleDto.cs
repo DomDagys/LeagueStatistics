f258ff013913835c45956c837b30d4b3338678e6
Pakaz1 <61023046+Pakaz1@users.noreply.github.com>
2020-03-28 19:30:46 +0200

    Role Dto

---
+++ LeagueStatisticsApi/LeagueStatistics/Dtos/SummonerDtos/RoleDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueStatistics.Dtos.SummonerDtos
+{
+    public class RoleDto
+    {
+        public string role { get; set; }
+        public int gameCount { get; set; }
+    }
+}
