5ca4f5126a3840c6a8b01a08112c30c5fcf92766
Pakaz1 <61023046+Pakaz1@users.noreply.github.com>
2020-03-28 19:30:02 +0200

    Champion Dto

---
+++ LeagueStatisticsApi/LeagueStatistics/Dtos/SummonerDtos/ChampionDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueStatistics.Dtos.SummonerDtos
+{
+    public class ChampionDto
+    {
+        public int championId { get; set; }
+        public int kills { get; set; }
+        public int assists { get; set; }
+        public int deaths { get; set; }
+        public int wins { get; set; }
+        public int loss { get; set; }
+        public int gamesPlayed { get; set; }
+        public double kda { get; set; }
+    }
+}
