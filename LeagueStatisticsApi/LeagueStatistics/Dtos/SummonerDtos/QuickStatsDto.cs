0d634b7d4b8ce38595e76c341268bbd4dbde7273
Pakaz1 <61023046+Pakaz1@users.noreply.github.com>
2020-03-28 19:30:35 +0200

    QuickStats Dto

---
+++ LeagueStatisticsApi/LeagueStatistics/Dtos/SummonerDtos/QuickStatsDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueStatistics.Dtos.SummonerDtos
+{
+    public class QuickStatsDto
+    {
+        public int wins { get; set; }
+        public int loss { get; set; }
+        public int gamesPlayed { get; set; } //Usually 10 unless player has <10
+        public List<ChampionDto> favoriteChampions { get; set; }
+        public List<RoleDto> favoriteRoles { get; set; }
+        public int kills { get; set; }
+        public int deaths { get; set; }
+        public int assists { get; set; }
+        public double kda { get; set; }
+    }
+}
