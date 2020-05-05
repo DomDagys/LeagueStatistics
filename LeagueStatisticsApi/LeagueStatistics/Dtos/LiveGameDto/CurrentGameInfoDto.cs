e3f7637443eae052b66368ddf06d8748d40a8547
Auris43 <paliliunas.a@gmail.com>
2020-05-05 16:54:14 +0300

    Fixed LiveGame info

--- LeagueStatisticsApi/LeagueStatistics/Dtos/LiveGameDto/CurrentGameInfoDto.cs
+++ LeagueStatisticsApi/LeagueStatistics/Dtos/LiveGameDto/CurrentGameInfoDto.cs
@@ -12,7 +12,7 @@ namespace LeagueStatistics.Dtos.LiveGameDto
         public long gameStartTime { get; set; }
         public long mapId { get; set; }
         public long gameLength { get; set; }
-        public long platformId { get; set; }
+        public string platformId { get; set; }
         public string gameMode { get; set; }
         public long gameQueueConfigId { get; set; }
         public List<BannedChampionDto> bannedChampions { get; set; }
