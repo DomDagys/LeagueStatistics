0c7f12fbcbe859ebd9169e596594ac3cba301b32
Auris43 <paliliunas.a@gmail.com>
2020-05-11 16:51:19 +0300

    Added champion mastery services and dto's

---
+++ LeagueStatisticsApi/LeagueStatistics/Services/Interfaces/IChampionMastery_V4Service.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LeagueStatistics.Dtos.ChampionMasteryDto;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueStatistics.Services.Interfaces
+{
+    public interface IChampionMastery_V4Service
+    {
+        List<ChampionMasteryListDto> ChampionMasteryBySummonerId(string summonerId, string region);
+    }
+}
